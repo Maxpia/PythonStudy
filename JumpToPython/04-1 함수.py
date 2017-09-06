91e588ccbaa08c42472d2e3ee39d3b76ec2cf979
Maxpia <28941470+Maxpia@users.noreply.github.com>
2017-09-06 19:56:05 +0900

    170906_1

---
+++ JumpToPython/04-1 함수.py
@@ -0,0 +1,9 @@
+# 04-1 함수.py
+def sey_muself(name, old, man=True):
+    print("나의 이름은 %s 입니다." % name)
+    print("나의 나이는 %d 입니다." % old)
+    if man:
+        print("나는 남자입니다.")
+    else:
+        print("나는 여자입니다.")
+
