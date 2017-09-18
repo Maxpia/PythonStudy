bbc4d5fdbc60da4eff0ae0f09e8e3fd365ceadb9
Maxpia <28941470+Maxpia@users.noreply.github.com>
2017-09-18 20:51:03 +0900

    tensorflow-1.3.0 offline package

---
+++ tensorflow-1.3.0/tensorflow-1.3.0_setup.bat
@@ -0,0 +1,21 @@
+python -m pip install py-1.4.34-py2.py3-none-any.whl
+python -m pip install colorama-0.3.9-py2.py3-none-any.whl
+REM #py, colorama
+python -m pip install pytest-3.2.2-py2.py3-none-any.whl
+python -m pip install wheel-0.30.0-py2.py3-none-any.whl
+python -m pip install six-1.11.0-py2.py3-none-any.whl
+python -m pip install protobuf-3.4.0-py2.py3-none-any.whl
+python -m pip install numpy-1.13.1-cp36-none-win_amd64.whl
+REM # html5lib-0.9999999.tar.gz	에서 설치 whl 없음
+python -m pip install html5lib-0.9999999
+python -m pip install bleach-1.5.0-py2.py3-none-any.whl
+REM # Markdown-2.6.8.tar.gz	에서 설치 whl 없음
+python -m pip install Markdown-2.6.8
+python -m pip install Werkzeug-0.12.2-py2.py3-none-any.whl
+REM # Markdown >= 2.6.8, Werkzeug >= 0.11.10
+python -m pip install tensorflow_tensorboard-0.1.6-py3-none-any.whl
+python -m pip install pbr-3.1.1-py2.py3-none-any.whl
+REM # pbr >= 0.11
+python -m pip install mock-2.0.0-py2.py3-none-any.whl
+python -m pip install backports.weakref-1.0.post1-py2.py3-none-any.whl
+python -m pip install tensorflow-1.3.0-cp36-cp36m-win_amd64.whl
